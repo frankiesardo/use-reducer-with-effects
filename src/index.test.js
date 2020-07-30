fd498a1b33e61c0ea7929524a6d0ea474bd78444
Francesco Sardo <fran.sardo@gmail.com>
2020-07-30 13:59:46 +0200

    Add test

--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,50 @@
 import { useReducerWithEffects } from './'
 import { renderHook, act } from "@testing-library/react-hooks";
 
-// mock timer using jest
-jest.useFakeTimers();
-
 describe('useReducerWithEffects', () => {
-  // it('updates every second', () => {
-  //   const { result } = renderHook(() => useMyHook());
+  it('should be defined', () => {
+    expect(useReducerWithEffects).toBeDefined()
+  })
 
-  //   expect(result.current).toBe(0);
+  it('should return state and dispatch', () => {
+    const hook = renderHook(() => useReducerWithEffects(() => { }, {}, {}))
 
-  //   // Fast-forward 1sec
+    expect(hook.result.current.length).toBe(2)
+    expect(typeof hook.result.current[0]).toBe('object')
+    expect(typeof hook.result.current[1]).toBe('function')
+  })
+
+  it('Updates state and executes side effects', () => {
+    function reducer(state, action) {
+      return {
+        state: { ...state, count: state.count + 1 },
+        documentTitle: "Updated Title"
+      }
+    }
+
+    let handlers = { documentTitle: (dispatch, payload) => document.title = payload }
+
+    const { result } = renderHook(() => useReducerWithEffects(reducer, handlers, { count: 0 }))
+
+    expect(document.title).toBe('')
+
+    act(() => {
+      result.current[1]({})
+    })
+
+    expect(result.current[0].count).toBe(1)
+
+    expect(document.title).toBe("Updated Title")
+  })
+
+  // it('should throw an error if the handler key is unknown', () => {
+
+  //   const { result } = renderHook(() => useReducerWithEffects(() => ({key2: '' }), { key1: () => { } }, {}))
+    
   //   act(() => {
-  //     jest.advanceTimersByTime(1000);
-  //   });
-
-  //   // Check after total 1 sec
-  //   expect(result.current).toBe(1);
-
-  //   // Fast-forward 1 more sec
-  //   act(() => {
-  //     jest.advanceTimersByTime(1000);
-  //   });
-
-  //   // Check after total 2 sec
-  //   expect(result.current).toBe(2);
+  //     result.current[1]({})
+  //   })
+    
+  //   expect(result.error).toEqual(Error("Unrecognised handler key key2"))
   // })
 })
